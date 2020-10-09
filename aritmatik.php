dec799a6f1a8a8c179e9593ad2f7c54634e3abea
Bisma Verdya Adhika <49088823+j3d118156@users.noreply.github.com>
2020-10-09 18:23:24 +0700

    Add files via upload

---
+++ aritmatik.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<body>
+  <form>
+  		<input type="number" name="bil1">
+        <select>
+        	<option value="+">+</option>
+            <option value="-">-</option>
+            <option value="x">x</option>
+            <option value="/">/</option>
+        </select>
+        <input type="number" name="bil2">
+        <input type="submit" name="sum" value="=">
+  </form>
+</body>
+</html>
